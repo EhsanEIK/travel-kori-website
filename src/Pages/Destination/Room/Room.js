c6fae32d85dc6bc8bac6762f8fbde157069fb48d
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-24 13:34:03 +0600

    updated responsive design on Destination page

--- src/Pages/Destination/Room/Room.js
+++ src/Pages/Destination/Room/Room.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Image } from 'react-bootstrap';
+import './Room.css';
 
 const Room = ({ room }) => {
     const { img, name, bed, person } = room;
@@ -7,7 +8,7 @@ const Room = ({ room }) => {
     return (
         <Col lg={12} md={6}>
             <div className='d-lg-flex border rounded-2 gap-3 p-4 ms-0 me-2 mb-3'>
-                <Image src={img} className='w-md-50 w-100 h-auto' />
+                <Image src={img} className='img-size h-auto' />
                 <div>
                     <h3>{name}</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quos officia, voluptatem obcaecati, animi numquam cumque facere natus mollitia in delectus pariatur unde repudiandae asperiores nihil atque nisi eos nostrum hic impedit quo iusto.</p>
